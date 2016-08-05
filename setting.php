0d62f14dde0f860b8c5cc509499a584010a0ffd6
istiakMahbub <fahim_aiub22@yahoo.com>
2016-08-06 03:28:23 +0600

    Create setting.php

---
+++ setting.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Setting page</title>
+</head>
+<body>
+<?php
+session_start();
+//echo "Hello Fahim";
+?>
+<a href="logout.php"><input type="button" name="logout" value="Sign Out" ></a>
+</body>
+</html>
