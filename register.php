dcc934a40bef213b4918ab6566930582f3e5f926
istiakMahbub <fahim_aiub22@yahoo.com>
2016-08-06 03:25:59 +0600

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Sign Up</title>
+	<title>Landing Page</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -10,64 +10,38 @@
 
 <form action="login.php" method="post">
      <p>Already have an account?</p>
-     <table>
-        <tr>
-            <td>
-                <input class="login-form" type="email" name="loginEmail" placeholder="Registerd Email">
-            </td>
 
-            <td>
-                <input class="login-form" type="password" name="loginPass" placeholder="Password">
-            </td>
-
-            <td>
-                <button class="login-form" type="submit" value="Sign IN"><b>Sign In</b></button>
-            </td>
-
-        </tr>
-     </table>
+        <input class="login-form" type="email" name="loginEmail" placeholder="Registerd Email">
+    
+        <input class="login-form" type="password" name="loginPass" placeholder="Password">
+    
+        <button class="login-form" type="submit" name="btnLogin" value="Sign IN"><b>Sign In</b></button>
+      
      </form>
 </div>
 
 
-<div class="sign-up-form">
+
+<div class="signUP">
             
-            <form action="config.php" method="post">
-                <table align="center" cellspacing="1">
-                    <tr>
-                        <td>
-                            <input class="text-input" type="text" name="name" placeholder="Full Name">
-                        </td>
-                    </tr>
+    <form action="config.php" method="post">
+       
+            
+        <input class="text-input" id="name" type="text" name="name" placeholder="Full Name" required maxlength="22">
+        <br>
+ 
 
-                    <tr>
-                        <td>
-                            <input class="text-input" type="email" name="email" placeholder="Email">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input class="text-input" type="password" name="pass" placeholder="Password">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input class="text-input" type="password" name="conPass" placeholder="Confirm Password">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <button class="submit text-input" type="submit" value="Sign up"><b>Sign Up</b></button>
-                        </td>
-                    </tr>     
-                </table>       
-            </form>
-        </div>
+        <input class="text-input" type="email" name="email" placeholder="Email" required><br>
+  
+    
+        <input class="text-input"  type="password" name="pass" placeholder="Password length between 5 to 10" required pattern=".{5,10}"><br>
 
-        
+ 
+        <input class="text-input" type="password"  name="conPass" placeholder="Confirm Password" required pattern=".{5,10}"><br>
 
+        <button class="submit text-input" type="submit" value="Sign up"><b>Sign Up</b></button><br>
+                     
+    </form>
+ </div>
 </body>
 </html>
